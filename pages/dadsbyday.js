aa22548975fbe2b43130a0b016dc985964760401
Johnni Mortensen <don@jo9.dk>
2020-02-03 16:17:21 +0100

    edit dbd names according to esportsliga hp

--- pages/dadsbyday.js
+++ pages/dadsbyday.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Team from '../components/team';
 
 const DadsByDay = () => (
-    <Team name="DadsByDay" members={["Anders -muruz- Buchholt", "Christian -LuskereN- Elkjær", "Kasper -wiLL- Andersen", "Peter -C1VO- Martinussen", "Thomas Elvis -Lommer- Smedegaard"]} />
+    <Team name="DadsByDay" members={["muruz", "LuskereN", "wiLL", "C1VO", "Lommer"]} />
 );
 
 export default DadsByDay;
