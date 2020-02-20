d0e191776e98a516a608a77f4fc7e1b84ccfb90b
Johnni Mortensen <jm@lscontrol.dk>
2020-02-20 14:35:59 +0100

    add dbd + legends members from liga

--- pages/dadsbyday.js
+++ pages/dadsbyday.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import Team from '../components/team';
+import fetch from "isomorphic-unfetch";
 
-const DadsByDay = () => (
-    <Team name="DadsByDay" members={["muruz", "LuskereN", "wiLL", "C1VO", "Lommer"]} />
-);
+const DadsByDay = ({team}) => {
+    const members = team.members.map(x => x.user.nickName)
+    return (
+        <Team name="DadsByDay" members={members} />
+    )
+}
+
+DadsByDay.getInitialProps = async ctx => {
+
+    const res = await fetch(
+        "https://app.esportligaen.dk/api/team/1659?includeViewInfo=true"
+    );
+    const json = await res.json();
+    return { team: json };
+};
 
 export default DadsByDay;
 
--- pages/legends.js
+++ pages/legends.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import Team from '../components/team';
- 
-const Legends = () => (
-    <Team name="Legends" members={[".que","West","is 8>","vnG","Bitfisk", "gIZZAR"]}/>
-);
- 
+import fetch from "isomorphic-unfetch";
+
+const Legends = ({team}) => {
+    const members = team.members.map(x => x.user.nickName)
+    return (
+        <Team name="Legends" members={members} />
+    )
+}
+
+Legends.getInitialProps = async ctx => {
+
+    const res = await fetch(
+        "https://app.esportligaen.dk/api/team/1649?includeViewInfo=true"
+    );
+    const json = await res.json();
+    return { team: json };
+};
 
 export default Legends;
